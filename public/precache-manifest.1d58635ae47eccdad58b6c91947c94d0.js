108344253f2e93574c87beaaf372751ef4b29504
Aleksandr Klimenkov <prisioner@mail.ru>
2019-12-02 06:05:06 +0300

    builded app

---
+++ public/precache-manifest.1d58635ae47eccdad58b6c91947c94d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f4a162e16520a428e9f1969828af7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce85cb9d50626d799b59",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4bd0e1826e57e8cea566",
+    "url": "/static/js/2.16b2656f.chunk.js"
+  },
+  {
+    "revision": "ce85cb9d50626d799b59",
+    "url": "/static/js/main.c5a1a517.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
---
+++ public/precache-manifest.ff85e3ea8b3be6e6e73d6cd4fd42af76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48e41b9414715e50a64c7fc4cb82316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f4bcce35b498c03ff8e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6fb9a908beb1a1ddd619",
+    "url": "/static/js/2.6bab3adb.chunk.js"
+  },
+  {
+    "revision": "1f4bcce35b498c03ff8e",
+    "url": "/static/js/main.9edc4f52.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
